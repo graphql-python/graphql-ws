e75d23da981f55807890771c7865462ae1255a40
Syrus Akbary <me@syrusakbary.com>
2017-11-16 23:49:21 -0800

    Update travis_pypi_setup.py

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'graphql-python/graphql_ws'
+GITHUB_REPO = 'graphql-python/graphql-ws'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
