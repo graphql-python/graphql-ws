de58dc908ac03d78a92b1dc13643a486a63007f4
hballard <heath.ballard@gmail.com>
2018-01-21 22:12:34 -0600

    Add pubsub classes for asyncio and rx

---
+++ graphql_ws/pubsub.py
@@ -0,0 +1,45 @@
+from asyncio import Queue
+from rx.subjects import Subject
+
+
+class AsyncioPubsub(object):
+
+    def __init__(self):
+        self.subscriptions = {}
+        self.sub_id = 0
+
+    async def publish(self, channel, payload):
+        if channel in self.subscriptions:
+            for q in self.subscriptions[channel].values():
+                await q.put(payload)
+
+    def subscribe_to_channel(self, channel):
+        self.sub_id += 1
+        q = Queue()
+        if channel in self.subscriptions:
+            self.subscriptions[channel][self.sub_id] = q
+        else:
+            self.subscriptions[channel] = {self.sub_id: q}
+        return self.sub_id, q
+
+    def unsubscribe(self, channel, sub_id):
+        if sub_id in self.subscriptions.get(channel, {}):
+            del self.subscriptions[channel][sub_id]
+
+
+class RxPubsub(object):
+
+    def __init__(self):
+        self.subscriptions = {}
+
+    def publish(self, channel, payload):
+        if channel in self.subscriptions:
+            self.subscriptions[channel].on_next(payload)
+
+    def subscribe_to_channel(self, channel):
+        if channel in self.subscriptions:
+            return self.subscriptions[channel]
+        else:
+            subject = Subject()
+            self.subscriptions[channel] = subject
+            return subject
