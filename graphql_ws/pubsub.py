2d28b5b853c7236bd358225453e648de4aa34ea6
hballard <heath.ballard@gmail.com>
2018-01-28 01:01:25 -0600

    Add AsyncioRedisPubsub class

--- graphql_ws/pubsub.py
+++ graphql_ws/pubsub.py
@@ -1,9 +1,10 @@
+import asyncio
 import pickle
 
+import aredis
 import gevent
 import redis
 
-from asyncio import Queue
 from rx.subjects import Subject
 
 
@@ -20,7 +21,7 @@ class AsyncioPubsub(object):
 
     def subscribe_to_channel(self, channel):
         self.sub_id += 1
-        q = Queue()
+        q = asyncio.Queue()
         if channel in self.subscriptions:
             self.subscriptions[channel][self.sub_id] = q
         else:
@@ -30,6 +31,8 @@ class AsyncioPubsub(object):
     def unsubscribe(self, channel, sub_id):
         if sub_id in self.subscriptions.get(channel, {}):
             del self.subscriptions[channel][sub_id]
+            if not self.subscriptions[channel]:
+                del self.subscriptions[channel]
 
 
 class RxPubsub(object):
@@ -48,6 +51,52 @@ class RxPubsub(object):
             subject = Subject()
             self.subscriptions[channel] = subject
             return subject
+
+    def unsubscribe(self, channel, disposable):
+        pass
+
+
+class AsyncioRedisPubsub(object):
+
+    def __init__(self, host='localhost', port=6379, *args, **kwargs):
+        self.redis = aredis.StrictRedis(host, port, *args, **kwargs)
+        self.pubsub = self.redis.pubsub(ignore_subscribe_messages=True)
+        self.subscriptions = {}
+        self.sub_id = 0
+        self.future = None
+
+    async def publish(self, channel, payload):
+        await self.redis.publish(channel, pickle.dumps(payload))
+
+    async def subscribe_to_channel(self, channel):
+        self.sub_id += 1
+        q = asyncio.Queue()
+        if channel in self.subscriptions:
+            self.subscriptions[channel][self.sub_id] = q
+        else:
+            await self.pubsub.subscribe(channel)
+            self.subscriptions[channel] = {self.sub_id: q}
+            if not self.future:
+                self.future = asyncio.ensure_future(
+                    self._wait_and_get_messages())
+        return self.sub_id, q
+
+    def unsubscribe(self, channel, sub_id):
+        if sub_id in self.subscriptions.get(channel, {}):
+            del self.subscriptions[channel][sub_id]
+            if not self.subscriptions[channel]:
+                self.pubsub.unsubscribe(channel)
+                del self.subscriptions[channel]
+
+    async def _wait_and_get_messages(self):
+        while True:
+            msg = await self.pubsub.get_message()
+            if msg:
+                channel = msg['channel'].decode()
+                if channel in self.subscriptions:
+                    for q in self.subscriptions[channel].values():
+                        await q.put(pickle.loads(msg['data']))
+            await asyncio.sleep(.001)
 
 
 class GeventRedisPubsub(object):
@@ -72,6 +121,9 @@ class GeventRedisPubsub(object):
             if not self.greenlet:
                 self.greenlet = gevent.spawn(self._wait_and_get_messages)
             return subject
+
+    def unsubscribe(self, channel, disposable):
+        pass
 
     def _wait_and_get_messages(self):
         while True:
