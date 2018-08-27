dc478beb1b495328bb220640e1762eed7083b5d2
Chris Beaven <smileychris@gmail.com>
2018-08-27 13:16:10 +1200

    Add session_application helper to django routing

--- graphql_ws/django/routing.py
+++ graphql_ws/django/routing.py
@@ -1,4 +1,5 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.sessions import SessionMiddlewareStack
 from django.apps import apps
 from django.urls import path
 from .consumers import GraphQLSubscriptionConsumer
@@ -12,6 +13,10 @@ else:
 websocket_urlpatterns = [path("subscriptions", GraphQLSubscriptionConsumer)]
 
 application = ProtocolTypeRouter({"websocket": URLRouter(websocket_urlpatterns)})
+
+session_application = ProtocolTypeRouter(
+    {"websocket": SessionMiddlewareStack(URLRouter(websocket_urlpatterns))}
+)
 
 if AuthMiddlewareStack:
     auth_application = ProtocolTypeRouter(
