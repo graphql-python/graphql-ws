eab7a2f69c297e646be28cd7133cc16d17586fe1
Christoph Zwerschke <cito@online.de>
2020-02-21 16:07:07 +0100

    Update requirements for tests

--- graphql_ws/__init__.py
+++ graphql_ws/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Syrus Akbary"""
 __email__ = 'me@syrusakbary.com'
-__version__ = '0.1.0'
+__version__ = '0.3.1'
 
 
 from .base import BaseConnectionContext, BaseSubscriptionServer
