f7ef09fef63b9a5408496988fe5292a78d6a5475
Chris Beaven <smileychris@gmail.com>
2020-06-06 17:08:24 +1200

    Add base async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import sys
 if sys.version_info > (3,):
     collect_ignore = ["test_django_channels.py"]
     if sys.version_info < (3, 6):
-        collect_ignore.append('test_gevent.py')
+        collect_ignore.append("test_gevent.py")
 else:
-    collect_ignore = ["test_aiohttp.py"]
+    collect_ignore = ["test_aiohttp.py", "test_base_async.py"]
